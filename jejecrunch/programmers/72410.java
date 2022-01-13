f63d98afd804357aaa36f4236f71bb8c5094298a
jejecrunch <gkswlgp456@gmail.com>
2022-01-13 22:54:25 +0900

    add: 리드미파일에 넣기 위해 임시로 푼 문제 추가

---
+++ jejecrunch/programmers/72410.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String new_id) {
+        String answer = "";
+        
+        answer = new_id.toLowerCase(); // 1단계
+        answer = answer.replaceAll("[^\\w\\-_.]*", ""); // 2단계
+        answer = answer.replaceAll("\\.{2,}", ".");// 3단계
+        answer = answer.replaceAll("^[.]|[.]$", ""); // 4단계
+        if(answer.length() == 0) answer = "a"; // 5단계
+        if(answer.length() >= 16) { // 6단계
+            answer = answer.substring(0, 15);
+            answer = answer.replaceAll("^[.]|[.]$", "");
+        }
+        if(answer.length() <= 2){ // 7단계
+            while(answer.length() < 3) {
+                answer = answer.concat(answer.substring(answer.length() - 1));
+            }
+        }
+
+        return answer;
+    }
+}
