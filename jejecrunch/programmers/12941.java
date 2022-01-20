978df37c1af0dbc80e34a79a35647632c5a087f2
제제jeje <gkswlgp456@gmail.com>
2022-01-20 20:52:40 +0900

    add: 12941 최솟값 만들기

---
+++ jejecrunch/programmers/12941.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution{
+    public int solution(int []A, int []B){
+        int answer = 0;
+        
+        Integer[] arr = Arrays.stream(B).boxed().toArray(Integer[]::new);
+
+        Arrays.sort(A);
+        Arrays.sort(arr, Collections.reverseOrder());
+        
+        for(int i=0;i<A.length;i++){
+            answer += A[i]*arr[i];
+        }
+
+        return answer;
+    }
+}
