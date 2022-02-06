8339c1801533720f95044cd37b79a98ea819b151
제제jeje <gkswlgp456@gmail.com>
2022-02-06 22:58:05 +0900

    add: 10989 수 정렬하기 3

---
+++ jejecrunch/BAEKJOON/SortNumber3_10989.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class SortNumber3_10989 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int[] cnt = new int[10001];
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            cnt[Integer.parseInt(br.readLine())]++;
+        }
+
+        br.close();
+
+        for (int i = 1; i < 10001; i++) {
+            while (cnt[i] > 0) {
+                sb.append(i).append('\n');
+                cnt[i]--;
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+}
